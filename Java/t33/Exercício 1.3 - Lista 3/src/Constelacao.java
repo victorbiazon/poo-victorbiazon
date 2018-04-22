b15e4659cc57e07566d4e9ba4d65e835cf7a2f46
GSFar <36648612+GSFar@users.noreply.github.com>
2018-04-22 13:09:36 -0300

    Update Constelacao.java

--- Java/t33/Exercício 1.3 - Lista 3/src/Constelacao.java
+++ Java/t33/Exercício 1.3 - Lista 3/src/Constelacao.java
@@ -2,45 +2,41 @@ import java.util.ArrayList;
 
 public class Constelacao {
 
-private ArrayList<Estrela> star;										//nomear o arraylist da classe Estrela
+private ArrayList<Estrela> star;										
     
     public Constelacao(){
-        this.star = new ArrayList<Estrela>();							//construtor que atribui os elementos que aparecem na classe Estrela
-    }																	//ao vetor star
+        this.star = new ArrayList<Estrela>();							
+    }																	
     
-    public void adicionar(Estrela e){									//adiciona ao vetor star todos os elementos elencados na classe Estrela
+    public void adicionar(Estrela e){									
         star.add(e);
     }
        
-    public void infoEstrelas(){											//exibe todas as informações sobre as estrelas nomeadas na classe principal(Teste)
+    public void infoEstrelas(){											
     	
-    	System.out.println("Informações das estrelas desta constelação: ");
-    	System.out.println();                                           //apenas para pular linha
-		for(Estrela e: star) {											//para cada índice do vetor star, são atribuídos os três atributos listados
-			e.mostrar();															//abaixo, extraídos da classe Estrela
-            //System.out.println("Nome: " + e.getNome());				    //recebe os valores do atributo "nome" da classe Estrela
-            //System.out.println("Temperatura: " + e.getTemp() + " K");	//recebe os valores do atributo "temp" da classe Estrela
-            //System.out.println("Cor: " + e.getCor());					//recebe os valores do atributo "cor" da classe Estrela
-            //System.out.println();                                      //apenas para pular linha			
-		}		
+    	System.out.println("InformaÃ§Ãµes das estrelas desta constelaÃ§Ã£o: ");
+    	System.out.println();                                           
+		for(Estrela e: star) {											
+			e.mostrar();		
+                }		
     }
     
     public double tempConstelacao(){
         double total = 0;
         for(Estrela e: star){											
-            total += e.getTemp();										//soma todas as temperaturas das estrelas elencadas na classe principal (Teste)
+            total += e.getTemp();										
         }
         return total;
     }
     
     public void mostraTempConstelacao() {
-    	System.out.println("A soma das temperaturas das estrelas elencadas é igual a " + tempConstelacao() + " K." );
+    	System.out.println("A soma das temperaturas das estrelas elencadas Ã© igual a " + tempConstelacao() + " K." );
     }
     
     public void introducao() {
-    	System.out.println("CONSTELAÇÃO URSA MAIOR");
+    	System.out.println("CONSTELAÃ‡ÃƒO URSA MAIOR");
     	System.out.println();										   //apenas para pular linha
-    	System.out.println("Lista de estrelas desta constelação, com suas respectivas temperaturas e cores: ");
+    	System.out.println("Lista de estrelas desta constelaÃ§Ã£o, com suas respectivas temperaturas e cores: ");
     	System.out.println();
     }
 }
