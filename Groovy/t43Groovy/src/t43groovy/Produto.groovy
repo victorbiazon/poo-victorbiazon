72683b400fef428aa60f8276d2a28ce2918c973c
GSFar <36648612+GSFar@users.noreply.github.com>
2018-05-15 21:49:14 -0300

    Update Produto.groovy

--- Groovy/t43Groovy/src/t43groovy/Produto.groovy
+++ Groovy/t43Groovy/src/t43groovy/Produto.groovy
@@ -1,23 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package t43groovy
-
-/**
- *
- * @author victo
- */
 class Produto {
 	
 	private String nome
 	private double preco
+	private Imposto imp
 	
-	Produto(String nome,double preco){
+	Produto(String nome,double preco,Imposto imp){
 		this.nome = nome
 		this.preco = preco
+		this.imp = imp
 	}
 
 	def getNome() {
@@ -26,6 +16,15 @@ class Produto {
 	
 	def getPreco() {
 		preco
+	}
+	
+	def getImp(){
+        imp
+	}
+	
+	def exibir(){
+        println ("Nome: " + getNome())
+        println ("Preco com imposto descontado: " + getPreco*imp.aliquota())
 	}
 
 }
