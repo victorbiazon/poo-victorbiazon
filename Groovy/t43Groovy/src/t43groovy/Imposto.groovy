46b3a449da4bb06de361eaff68d03dc5ded5e4f9
GSFar <36648612+GSFar@users.noreply.github.com>
2018-05-15 21:48:37 -0300

    Update Imposto.groovy

--- Groovy/t43Groovy/src/t43groovy/Imposto.groovy
+++ Groovy/t43Groovy/src/t43groovy/Imposto.groovy
@@ -1,20 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package t43groovy
-
-/**
- *
- * @author victo
- */
-class Imposto extends Produto {
-
-	Imposto(String nome,double preco){
-		super(nome,preco)
-	}
+class Imposto {
 	
 	def aliquota() {
 		1.0
