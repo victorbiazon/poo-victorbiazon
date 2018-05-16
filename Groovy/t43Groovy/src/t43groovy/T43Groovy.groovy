76fcbd0d409c5945aac2a800533c8e80aee6569a
GSFar <36648612+GSFar@users.noreply.github.com>
2018-05-15 21:50:11 -0300

    Update T43Groovy.groovy

--- Groovy/t43Groovy/src/t43groovy/T43Groovy.groovy
+++ Groovy/t43Groovy/src/t43groovy/T43Groovy.groovy
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package t43groovy
-
-/**
- *
- * @author victo
- */
 class T43Groovy {
     public static void main(String[] args) {
-        ICMS i1 = new ICMS("Chá",10)
-        ICMS i2 = new ICMS("Bolacha",4)
-        IPI ip1 = new IPI("Carne",27)
-        IPI ip2 = new IPI("Café",13)
+        Produto p1 = new Produt("Chá",10,new ICMS())
+        Produto p2 = new Produto("Bolacha",4,new ICMS())
+        Produto p3 = new Produto("Carne",27,new IPI())
+        Produto p4 = new Produto("Café",13,new IPI())
     
         Colecao c = new Colecao()
         c.adicionar(i1)
