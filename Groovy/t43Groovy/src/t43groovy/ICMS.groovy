e1186ff86a9a5351619c7a0f40cb4d2d9cb7eef2
GSFar <36648612+GSFar@users.noreply.github.com>
2018-05-15 21:47:06 -0300

    Update ICMS.groovy

--- Groovy/t43Groovy/src/t43groovy/ICMS.groovy
+++ Groovy/t43Groovy/src/t43groovy/ICMS.groovy
@@ -1,28 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package t43groovy
-
-/**
- *
- * @author victo
- */
 class ICMS extends Imposto {
-		
-	ICMS(String nome,double preco){
-		super(nome,preco)
-	}
 	
 	def aliquota() {
-		return getPreco() - getPreco()*0.10
+		return 0.9
 	}
-	
-	def exibir() {
-		System.out.println("Nome: " + getNome())
-		System.out.println("Preço sem imposto: " + aliquota())
-	}	
 }
-
