c0b72e746127d8dc79e50afd545e351e0d7b3a1b
GSFar <36648612+GSFar@users.noreply.github.com>
2018-05-15 21:46:15 -0300

    Update Colecao.groovy

--- Groovy/t43Groovy/src/t43groovy/Colecao.groovy
+++ Groovy/t43Groovy/src/t43groovy/Colecao.groovy
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package t43groovy
-
-/**
- *
- * @author victo
- */
 class Colecao {
 
 	private ArrayList<Produto> prod
@@ -24,10 +12,7 @@ class Colecao {
 	
 	def infoProduto() {
 		for(Produto p: prod) {
-			if(p instanceof ICMS)
-				((ICMS) p).exibir()
-			else
-				((IPI) p).exibir()		
+			p.exibir()	
 		}
 	}
 }
