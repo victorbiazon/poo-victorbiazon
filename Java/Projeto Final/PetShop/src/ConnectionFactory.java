ce9e09243d2918c149a891edcd6520e383b0586f
victorbiazon <36648570+victorbiazon@users.noreply.github.com>
2018-06-13 23:17:38 -0300

    Update ConnectionFactory.java

--- Java/Projeto Final/PetShop/src/ConnectionFactory.java
+++ Java/Projeto Final/PetShop/src/ConnectionFactory.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 	
 	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","0310vab");
+		return DriverManager.getConnection("jdbc:postgresql://10.66.32.4:5107/postgres","postgres","123");
 	}
 }
 
